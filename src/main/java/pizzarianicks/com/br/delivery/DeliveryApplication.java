d7e3bb6376ba2a439fac20067543cb0dd28b22b0
pedhros <pedrooohsss@gmail.com>
2024-08-04 12:37:42 -0300

    implementa api de teste

--- src/main/java/pizzarianicks/com/br/delivery/DeliveryApplication.java
+++ src/main/java/pizzarianicks/com/br/delivery/DeliveryApplication.java
@@ -2,12 +2,20 @@ package pizzarianicks.com.br.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class DeliveryApplication {
-
+	@GetMapping
+	public String getHomeTeste() {
+		return "Delivery - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
 	}
-
 }
