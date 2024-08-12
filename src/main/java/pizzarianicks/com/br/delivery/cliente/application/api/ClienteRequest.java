3915984d276a0b79e5f95c7326db578e9b8dcd83
pedhros <pedrooohsss@gmail.com>
2024-08-12 20:53:31 -0300

    implementa request e response

--- src/main/java/pizzarianicks/com/br/delivery/cliente/application/api/ClienteRequest.java
+++ src/main/java/pizzarianicks/com/br/delivery/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,23 @@
 package pizzarianicks.com.br.delivery.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@Email
+	@NotBlank
+	@Column(unique = true)
+	private String email;
+	@NotBlank
+	private String telefone;
+	@NotNull
+	private LocalDateTime dataNascimento;
 }
