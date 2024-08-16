28cb15f130217642cd285bd13589021684403e7f
pedhros <pedrooohsss@gmail.com>
2024-08-15 23:03:57 -0300

    implementa getTodos, teste ok!

--- src/main/java/pizzarianicks/com/br/delivery/cliente/application/api/ClienteListResponse.java
+++ src/main/java/pizzarianicks/com/br/delivery/cliente/application/api/ClienteListResponse.java
@@ -2,15 +2,28 @@ package pizzarianicks.com.br.delivery.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import lombok.Value;
 import pizzarianicks.com.br.delivery.cliente.domain.Cliente;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
 	private String email;
 	private String telefone;
+	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.email = cliente.getEmail();
+		this.telefone = cliente.getTelefone();
 	}
 }
